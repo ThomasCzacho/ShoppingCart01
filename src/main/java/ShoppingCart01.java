7b4a53859432ed5fd67cf8dff081358819dd606f
Thomas Czachorowski <Thomas.Czachorowski@cdpipelinedevshop.com>
2022-08-17 14:03:02 -0400

    Initial commit

---
+++ src/main/java/ShoppingCart01.java
@@ -0,0 +1,17 @@
+public class ShoppingCart01 {
+    public static void main(String[] args) {
+        // Declare and initialize String variables.  Do not initialize message yet.
+     String custName;
+     String itemDesc;
+     String message;
+
+
+        // Assign the message variable
+     custName="Daniel";
+     itemDesc="Shirt";
+     message=custName + " Wants to purchase a " + itemDesc;
+
+        // Print and run the code
+        System.out.println(message);
+    }
+}
